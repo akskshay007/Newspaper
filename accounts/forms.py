fcc00211ad5e6b21be19b0653435e6dbcbdf0cf6
akskshay007 <init.defaultBranch>
2022-07-30 18:18:35 +0530

    initial commit

---
+++ accounts/forms.py
@@ -0,0 +1,24 @@
+from distutils.sysconfig import customize_compiler
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = {
+            "username",
+            "email",
+            "age",
+        }
+        # fields = UserCreationForm.Meta.fields + ('age',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = {
+            "username",
+            "email",
+            "age",
+        }
+        # fields = UserChangeForm.Meta.fields
